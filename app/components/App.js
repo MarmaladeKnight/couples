f684a189cbe7597e13a3e82ea77beae68d5cb2be
MarmaladeKnight <misha06052000@gmail.com>
2018-06-09 19:26:41 +0300

    Default settings added(Webpack + Gulp + Babel + React)

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Card from './Card/Card.js'
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+            <Card />
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ app/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Card extends Component {
+
+    render() {
+        return (
+            <div>
+                Hello world qwdqd
+            </div>
+        )
+    }
+}
+
+export default Card
